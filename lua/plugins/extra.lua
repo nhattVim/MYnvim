b94ccfb143f0dcca9efca5e1fbf73a2fa3d5a733
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-11-16 17:52:16 +0700

    update

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -30,6 +30,11 @@ return {
         opts = {},
     },
     {
+        "glacambre/firenvim",
+        build = ":call firenvim#install(0)",
+        event = "VeryLazy",
+    },
+    {
         "windwp/nvim-ts-autotag", -- Auto tagging html tag
         ft = "html",
         opts = {},
