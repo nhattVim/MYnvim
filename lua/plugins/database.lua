f213b47072319bcfb0468a0143d37c1ac845785d
nhattvim <nhattruong13112000@gmail.com>
2024-11-30 08:15:40 +0000

    config mysql

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -25,16 +25,27 @@ return {
     end,
     config = function()
         vim.g.dbs = {
-            { name = "root", url = "mysql://root@localhost" },
-            { name = "albedo", url = "mysql://albedo@localhost" },
-            -- export MYSQL_PWD='1234567890p*'
+            -- sudo mysql -u root -p
+            -- ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'root';
+            -- FLUSH PRIVILEGES;
+            -- EXIT;
+            { name = "root", url = "mysql://root:root@localhost" },
+
+            -- sudo mysql -u root -p
+            -- CREATE USER 'albedo'@'localhost' IDENTIFIED BY 'albedo';
+            -- GRANT ALL PRIVILEGES ON *.* TO 'albedo'@'localhost' WITH GRANT OPTION;
+            -- FLUSH PRIVILEGES;
+            -- EXIT;
+            { name = "albedo", url = "mysql://albedo:albedo@localhost" },
         }
+
         vim.api.nvim_create_autocmd("FileType", {
             pattern = {
                 "sql",
             },
             command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
         })
+
         vim.api.nvim_create_autocmd("FileType", {
             pattern = {
                 "sql",
@@ -52,6 +63,20 @@ return {
                 else
                     vim.notify("Error loading cmp: " .. cmp, vim.log.levels.ERROR)
                 end
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "dbui",
+            callback = function()
+                vim.api.nvim_buf_set_keymap(
+                    0,
+                    "n",
+                    "s",
+                    "<Plug>(DBUI_SelectLineVsplit)",
+                    { noremap = true, silent = true }
+                )
+                vim.api.nvim_buf_set_keymap(0, "n", "v", "<Plug>(DBUI_SaveQuery)", { noremap = true, silent = true })
             end,
         })
     end,
