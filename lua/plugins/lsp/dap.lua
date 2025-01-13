ce75230dabb0dda9b847f8adbfd1eb095ae82d95
nhattvim <nhattruong13112000@gmail.com>
2025-01-13 12:44:32 +0000

    update

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -48,18 +48,32 @@ return {
             },
         })
 
-        vim.fn.sign_define("DapBreakpoint", { text = "󰨰", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
-        vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
+        vim.fn.sign_define("DapBreakpoint", {
+            text = "󰨰",
+            texthl = "DiagnosticSignError",
+            linehl = "",
+            numhl = "",
+        })
+
+        vim.fn.sign_define("DapStopped", {
+            text = "▶️",
+            texthl = "",
+            linehl = "",
+            numhl = "",
+        })
 
         dap.listeners.before.attach.dapui_config = function()
             dapui.open()
         end
+
         dap.listeners.before.launch.dapui_config = function()
             dapui.open()
         end
+
         dap.listeners.before.event_terminated.dapui_config = function()
             dapui.close()
         end
+
         dap.listeners.before.event_exited.dapui_config = function()
             dapui.close()
         end
