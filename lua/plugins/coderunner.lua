7f14ed54e1fb0f08516ffb310050b72557f46b0b
nhattvim <nhattruong13112000@gmail.com>
2025-07-23 19:12:52 +0700

    fix(coderunner): update command strings for cpp, c, rust, and java to use temp directory

--- lua/plugins/coderunner.lua
+++ lua/plugins/coderunner.lua
@@ -1,45 +1,54 @@
+local temp = vim.fn.stdpath("cache") .. "/out"
+vim.fn.mkdir(temp, "p")
+
 return {
     "CRAG666/code_runner.nvim",
     cmd = { "RunCode", "RunFile", "RunProject" },
     opts = {
         filetype = {
-            java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
-            cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir/$fileNameWithoutExt",
-            c = "cd $dir && gcc $fileName -o $fileNameWithoutExt && $fileNameWithoutExt",
+            cpp = string.format(
+                "cd $dir && g++ $fileName -o '%s/$fileNameWithoutExt' && '%s/$fileNameWithoutExt'",
+                temp,
+                temp
+            ),
+            c = string.format(
+                "cd $dir && gcc $fileName -o '%s/$fileNameWithoutExt' && '%s/$fileNameWithoutExt'",
+                temp,
+                temp
+            ),
+            rust = string.format(
+                "cd $dir && rustc $fileName -o '%s/$fileNameWithoutExt' && '%s/$fileNameWithoutExt'",
+                temp,
+                temp
+            ),
+            java = string.format(
+                "cd $dir && javac -d '%s' $fileName && cd '%s' && java $fileNameWithoutExt",
+                temp,
+                temp
+            ),
             python = "python3 -u",
             typescript = "deno run",
             javascript = "node $dir/$fileName",
             dart = "dart $fileName",
-            rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
             scss = "sass $dir/$fileName $dir/$fileNameWithoutExt.css",
             lua = "cd $dir && lua $fileName",
         },
-        mode = "float", -- "float", "term", "tab"
+        mode = "float",
         focus = true,
         startinsert = true,
         term = {
-            -- position = "vert",
             position = "bot",
             size = 10,
         },
         float = {
-            -- Key that close the code_runner floating window
             close_key = "<ESC>",
-            -- Window border (see ':h nvim_open_win')
-            -- border = "solid",
             border = "rounded",
-
-            -- Num from `0 - 1` for measurements
             height = 0.8,
             width = 0.8,
             x = 0.5,
             y = 0.5,
-
-            -- Highlight group for floating window/border (see ':h winhl')
             border_hl = "FloatBorder",
             float_hl = "Normal",
-
-            -- Transparency (see ':h winblend')
             blend = 0,
         },
     },
