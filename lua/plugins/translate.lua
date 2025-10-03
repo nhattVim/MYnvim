e5243a3400a17b8c6c94cf59367422eefbf12ce4
nhattvim <nhattruong13112000@gmail.com>
2025-10-03 20:13:15 +0700

    increase translate shell menu index

--- lua/plugins/translate.lua
+++ lua/plugins/translate.lua
@@ -3,5 +3,14 @@ return {
     event = "VeryLazy",
     opts = {
         default = { command = "translate_shell" },
+        preset = {
+            output = {
+                floating = {
+                    border = "rounded",
+                    filetype = "translate",
+                    zindex = 500,
+                },
+            },
+        },
     },
 }
