115e98499965dd00296acb0aa646e44493c9aa83
nhattvim <nhattruong13112000@gmail.com>
2026-05-05 15:28:03 +0700

    docs: edit README header and configure tree-sitter-manager plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,5 +3,17 @@ return {
     event = "VeryLazy",
     opts = {
         auto_install = true,
+        ensure_installed = {
+            "lua",
+            "markdown",
+            "html",
+            "css",
+            "javascript",
+            "typescript",
+            "tsx",
+            "json",
+            "cpp",
+            "java",
+        },
     },
 }
